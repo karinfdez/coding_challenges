ee54f5c5be61c1d02e0ade65ec9f6ee2f95116b1
Karin Fernandez <karinfdez@yahoo.com>
2016-01-21 14:01:12 -0500

    modified the option to ignore all non-alphanumeric characters

--- anagram.rb
+++ anagram.rb
@@ -1,14 +1,19 @@
 #Any word or phrase that exactly reproduces the letters in another order is an anagram."
 #(Wikipedia). Add numbers to this definition to be more interest.
-
+#The challenge is to write the function isAnagram to return True if the word 
+#test is an anagram of the word original and False if not.
+#Note: Anagrams are case insensitive, ignore all non-alphanumeric characters,
+#input will always be two strings. 
 
 def isAnagram?(test,original)
 
     isBlock=proc {
     	test.downcase.chars.sort!.join==original.downcase.chars.sort!.join
     }
-	test.gsub!(/\s+/, "")
-	original.gsub!(/\s+/, "")
+    #Remove all non-alphanumeric characters.
+    test.gsub!(/\W+/, '')
+    original.gsub!(/\W+/, '')	
+    
 	test.length==original.length ? isBlock.call : false 
 end
 
